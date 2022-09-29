31b9712fed40059a59bd74c9cc3f128d6f9185fe
Ozan Çiçek <77584235+ozancck@users.noreply.github.com>
2022-09-29 16:21:01 +0300

    landmarks

--- LandmarkBook/ViewController.swift
+++ LandmarkBook/ViewController.swift
@@ -7,11 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        var landmarkNames = [String]()
+        landmarkNames.append("Anıtkabir")
+        landmarkNames.append("Egypt")
+        landmarkNames.append("statue Of Liberty")
+        landmarkNames.append("Tower Of Pisa")
+        
+        var landmarkImages = [UIImage]()
+        
+        landmarkImages.append(UIImage(named: "anitkabir")!)
+        landmarkImages.append(UIImage(named: "Egypt")!)
+        landmarkImages.append(UIImage(named: "statueOfLiberty")!)
+        landmarkImages.append(UIImage(named: "TowerofPisa")!)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        var content = cell.defaultContentConfiguration()
+        
+        content.text = "ozan"
+        content.secondaryText="seses"
+        
+        cell.contentConfiguration = content
+        return cell
+        
     }
 
 
